47b8731cba45ac231286f5b23d4f11753749c785
dreevo <sidedark85@gmail.com>
2024-09-25 18:16:17 +0100

    add edge service with circuit breaker redis request limiter

---
+++ src/main/java/com/bookshop/bookshopedgeservice/BookshopEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookshop.bookshopedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopEdgeServiceApplication.class, args);
+    }
+
+}
